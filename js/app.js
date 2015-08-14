cf127448a16f557038a188f3be0bb92f911192e2
Su7ech <tpbaroody@gmail.com>
2015-08-14 10:48:38 -0700

    Finished Fizzbuzz Refactor

--- js/app.js
+++ js/app.js
@@ -1,8 +1,38 @@
-var userInput = prompt("Enter a number:");
-while (!(Number.isInteger(parseInt(userInput)))) {
-	var userInput = prompt("That's not a recognized number, try again...");
-}
+// var userInput = prompt("Enter a number:");
+// while (!(Number.isInteger(parseInt(userInput)))) {
+// 	var userInput = prompt("That's not a recognized number, try again...");
+// }
 
+function getUserInput() {
+	var element = document.getElementById('numInput');
+	var text = element.value;
+	var num = parseInt(text, 10);
+	if (isValidNumber(num)) {
+		resetLayout();
+		fizzbuzz(num);
+		element.value = '';
+	} else {
+		alert("Not a valid number, try again...");
+		element.value = '';
+	}
+}
+function resetLayout() {
+	var currentDiv = document.getElementById("fizzbuzz");
+	while (currentDiv.firstChild) {
+		currentDiv.removeChild(currentDiv.firstChild);
+	}
+}
+function isValidNumber(n) {
+	return Number.isInteger(n);
+}
+function addFizzbuzz(n) {
+	var p = document.createElement("p");
+	var newContent = document.createTextNode(n);
+	p.appendChild(newContent);
+
+	var currentDiv = document.getElementById("fizzbuzz");
+	currentDiv.appendChild(p);
+}
 function fizzbuzz(num) {
 	function check(n) {
 		var msg = '';
@@ -10,8 +40,7 @@ function fizzbuzz(num) {
 		if (i % 5 == 0) {msg += 'Buzz'};
 		return msg || n;
 	}
-	for (var i = 1; i <= userInput; i++) {
-		document.write("<p>" + check(i) + "</p>");
+	for (var i = 1; i <= num; i++) {
+		addFizzbuzz(check(i));
 	}
 }
-fizzbuzz(userInput);
